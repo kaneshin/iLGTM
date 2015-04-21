2662d9d908707b6e6e778ab8ad7f3b95e011c78a
Shintaro Kaneko <kaneshin0120@gmail.com>
2015-04-21 14:43:36 +0900

    Initial commit

---
+++ Podfile
@@ -0,0 +1,9 @@
+source 'https://github.com/CocoaPods/Specs.git'
+
+platform :ios, '8.0'
+inhibit_all_warnings!
+
+xcodeproj 'iLGTM'
+
+link_with 'iLGTM'
+pod 'CTAssetsPickerController',  '~> 2.9.0'
---
+++ iLGTM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iLGTM
+//
+//  Created by Shintaro Kaneko on 4/21/15.
+//  Copyright (c) 2015 Shintaro Kaneko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
